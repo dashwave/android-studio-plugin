904bfc6a53864ded272412e443a7a22a74d046e6
Aviral Jain <avi.aviral140@gmail.com>
2023-11-25 00:20:21 +0530

    update plugin file

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
     id("org.jetbrains.intellij") version "1.13.3"
 }
 
-group = "com.example"
-version = "1.0-SNAPSHOT"
+group = "com.dashwave"
+version = "1.0"
 
 repositories {
     mavenCentral()
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,4 +5,4 @@ pluginManagement {
     }
 }
 
-rootProject.name = "plugin1"
+rootProject.name = "plugin"
